311715a52f1a59d6fe40f9a0bb4ac1be97602ff6
Милана Хаметова (2007) <ilucamel200701@gmail.com>
2026-01-19 17:08:54 +0500

    Update Point.java

--- Point.java
+++ Point.java
@@ -8,6 +8,22 @@ public class Point {
         this.y = y;
     }
 
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
     @Override
     public String toString() {
         return "{" + x + ";" + y + "}";
