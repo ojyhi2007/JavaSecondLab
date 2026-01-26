9b3bd69370283f8cfe6a824f69f051103708e1c6
Милана Хаметова (2007) <ilucamel200701@gmail.com>
2026-01-27 01:29:34 +0500

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Student {
+
+    private String name;
+    private int[] grades;
+
+    public Student(String name, int[] grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public int[] getGrades() {
+        return grades;
+    }
+
+    public void setGrades(int[] grades) {
+        this.grades = grades;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + Arrays.toString(grades);
+    }
+}
+
